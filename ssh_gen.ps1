07d4a661443c9c93ca00dabea94513ae81c615e1
Nicholas <nicholas.geoffrion@me.com>
2022-12-24 21:38:35 -0500

    Added copy to remote and correct default cat

--- ssh_gen.ps1
+++ ssh_gen.ps1
@@ -1,4 +1,9 @@
+# enable agent
 Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
+# start agent immediately
 start-ssh-agent.cmd
-ssh-keygen -t ed25519 -C "Git-Demo"
-cat path\to\ssh\key.pub
+# generate key
+ssh-keygen -t ed25519 
+# 
+# cat ~/.ssh/id_rsa.pub
+# scp ~/.ssh/id_rsa.pub remote.host:~/.ssh/
